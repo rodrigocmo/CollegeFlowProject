2a052664c2585059d00f97c60c6d834979b29e5f
Rodrigo Morais <rcastromorais01@gmail.com>
2023-10-05 15:18:16 -0300

    Start project

---
+++ src/main/java/com/flow/collegeflowproject/CollegeFlowProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flow.collegeflowproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeFlowProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeFlowProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flow/collegeflowproject/CollegeFlowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.collegeflowproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeFlowProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
