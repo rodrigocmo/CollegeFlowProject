b13f6c578bd1debe3bd2010b6ca6cd6800a7920a
Rodrigo Morais <rcastromorais01@gmail.com>
2023-10-06 15:03:21 -0300

    Start with WebClient

--- src/main/java/com/flow/collegeflowproject/external/WebClientRequests.java
+++ src/main/java/com/flow/collegeflowproject/external/WebClientRequests.java
@@ -1,23 +1,48 @@
 package com.flow.collegeflowproject.external;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+
+import java.time.Duration;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 
 @Configuration
 public class WebClientRequests {
 
 
     public void testWebClient(){
+
+
+
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+
+
         WebClient client = WebClient.builder()
                 .baseUrl("http://localhost:8080")
-                .defaultCookie("cookieKey", "cookieValue")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8080"))
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
+
+
+
+
     }
 
 }
