f71084988ec76cd24c257bcd716ea5d8a2cf8268
Rodrigo Morais <rcastromorais01@gmail.com>
2023-10-17 20:13:31 -0300

    testing webclient

--- src/main/java/com/flow/collegeflowproject/external/WebClientRequests.java
+++ src/main/java/com/flow/collegeflowproject/external/WebClientRequests.java
@@ -1,19 +1,26 @@
 package com.flow.collegeflowproject.external;
 
+import com.flow.collegeflowproject.db.Classroom;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 
@@ -21,7 +28,7 @@ import java.util.concurrent.TimeUnit;
 public class WebClientRequests {
 
 
-    public void testWebClient(){
+    public void testWebClient(Classroom classroom){
 
 
 
@@ -40,6 +47,14 @@ public class WebClientRequests {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
 
+        client.post().body(Mono.justOrEmpty(Optional.of(List.of(1,2))), Integer.class);
+
+
+        WebClient teste = WebClient.create();
+
+        teste.method(HttpMethod.GET)
+                .uri("/class/%d".formatted(classroom.getId(),"/class/%d".formatted(classroom.getId()))).
+                header("Content-Type", "application/json");
 
 
 
